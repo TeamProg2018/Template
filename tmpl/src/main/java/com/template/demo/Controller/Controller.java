4cbd9de3321aa9efabe18d9563bec916c1e22a46
TeamProg2018 <stanislavp024@gmail.com>
2019-02-28 15:38:51 +0200

    repo45

--- tmpl/src/main/java/com/template/demo/Controller/Controller.java
+++ tmpl/src/main/java/com/template/demo/Controller/Controller.java
@@ -1,8 +1,11 @@
 package com.template.demo.Controller;
 
 import com.template.demo.Entity.Goods;
+import com.template.demo.Entity.Market;
 import com.template.demo.Repository.GoodsRepository;
+import com.template.demo.Repository.MarketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,35 +15,74 @@ import java.util.Map;
 @org.springframework.stereotype.Controller
 public class Controller {
 
-//    @GetMapping("/index")
-//    public String getIndex(){
-//        return "index";
-//    }
+
 
     @Autowired
     GoodsRepository goodsRepository;
-
+@Autowired
+    MarketRepository marketRepository;
 
     @GetMapping("/gs")
-    public String getGoods(Map<String, Object> model) {
-        model.put("getAllGoods", goodsRepository.findAll());
+    public String getGoods() {
+
         return "market";
     }
 
-    @PostMapping("/gs")
+
+    @PostMapping("/am")
+    public String postMarket(@RequestParam String name) {
+
+        Market mk=new Market(name);
+        marketRepository.save(mk);
+
+        return "market";
+    }
+
+
+    @PostMapping("/ag")
     public String addGoods(
             Map<String, Object> model,
-            @RequestParam(value = "FormName") String setFromFormName,
-            @RequestParam(value = "FormPrice") Float setFromFormPrice) {
+            @RequestParam(value = "name") String setFromFormName,
+            @RequestParam(value = "price") Float setFromFormPrice,
+            @RequestParam(value="market") String market) {
         Goods goods = new Goods();
         goods.setName(setFromFormName);
         goods.setPrice(setFromFormPrice);
 
+
+
+       Market mk= marketRepository.findByName(market);
+        goods.setMarket(mk);
         goodsRepository.save(goods);
-        model.put("getAllGoods", goodsRepository.findAll());
+        return "market";
+    }
+
+
+
+    @PostMapping("/chm")
+    public String changeMarket(
+                              @RequestParam(value = "good") String good,
+                              @RequestParam(value="market") String market) {
+
+      Goods g=goodsRepository.findByName(good);
+      g.setMarket(marketRepository.findByName(market));
+      goodsRepository.save(g);
 
         return "market";
     }
+
+
+    @PostMapping("/sha")
+    public String showAll(@RequestParam(value="market") String market,
+                          Model model) {
+
+   //model.addAttribute("getAllGoods",marketRepository.findByName(market).getGoodsList());
+   model.addAttribute("getAllGoods",goodsRepository.findAll());
+
+      //  System.out.println(marketRepository.findByName(market).getGoodsList().get(0).getName());
+        return "market";
+    }
+
 }
 
 
